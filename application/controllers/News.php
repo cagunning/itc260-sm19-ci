94ec38a3849b5260da63c1ebfb7a9eb9a57139f3
cagunning <shelfnunny@gmail.com>
2019-07-21 16:12:30 -0700

    Custom-Titles/makeLinks()

--- application/controllers/News.php
+++ application/controllers/News.php
@@ -5,12 +5,21 @@ class News extends CI_Controller {
         public function __construct()
         {
                 parent::__construct();
+                $this->config->set_item('banner','News Section');
                 $this->load->model('news_model');
                 $this->load->helper('url_helper');
         }
 
         public function index()
         {
+                $this->config->set_item('title','Seattle Sports News');
+
+                $nav1 = $this->config->item('nav1');
+
+                // var_dump($nav1);
+                // die;
+
+
                 $data['news'] = $this->news_model->get_news();
                 $data['title'] = 'News archive';
                 $this->load->view('news/index', $data);
@@ -19,6 +28,24 @@ class News extends CI_Controller {
 
         public function view($slug = NULL)
         {
+                /*
+                        slug, without dashes
+                        uppercase slug words
+                        use dash with slug for the title
+                        maybe add, 'News flash - '
+
+
+                */
+
+                //slug without dashes 
+                $dashless_slug = str_replace("-"," ", $slug);
+
+                //upppercase slug words
+                $dashless_slug = ucwords($dashless_slug);
+
+                // use dash with slug for the title
+
+                $this->config->set_item('title','News Flash - ' . $dashless_slug);
                 $data['news_item'] = $this->news_model->get_news($slug);
 
                 if (empty($data['news_item']))
